0bcc2c2128101eac672dac82a7c87c48e5d307e7
Justin <apolongjustin49@gmail.com>
2026-01-05 11:45:42 +0800

    added a src folder and a database folder and a db.js file

---
+++ backend/src/database/db.js
@@ -0,0 +1,14 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_NAME
+});
+
+export default pool;
